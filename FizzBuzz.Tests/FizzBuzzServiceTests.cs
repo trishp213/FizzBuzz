befd765407213823702cb1497bc4d5307127bbab
Trish Polay <trish.polay@gmail.com>
2018-02-05 21:28:34 -0700

    TP-002: Converted to NUnit, Added more unit tests, Added validation

--- FizzBuzz.Tests/FizzBuzzServiceTests.cs
+++ FizzBuzz.Tests/FizzBuzzServiceTests.cs
@@ -1,56 +1,56 @@
-﻿using System.Linq;
-using Domain;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System;
+using System.IO;
+using System.Linq;
+using FizzBuzz.Domain;
+using NUnit.Framework;
 
 namespace FizzBuzz.Tests
 {
-    [TestClass]
+    [TestFixture]
     public class FizzBuzzServiceTests
     {
         private FizzBuzzService _fizzBuzzService;
 
-        [TestInitialize]
+        [SetUp]
         public void Setup()
         {
             _fizzBuzzService = new FizzBuzzService();
         }
 
-        [TestMethod]
+        /*********************************************************
+         * Word replacements tests
+         * *******************************************************/
+        [Test]
         public void ShouldReturnFizzForNumber3()
         {
-            _fizzBuzzService.SetToDefaultValues();        
             var result = _fizzBuzzService.GetValueForNumber(3);
             Assert.AreEqual(result, "Fizz");
         }
 
-        [TestMethod]
+        [Test]
         public void ShouldReturnBuzzForNumber5()
         {
-            _fizzBuzzService.SetToDefaultValues();
             var result = _fizzBuzzService.GetValueForNumber(5);
             Assert.AreEqual(result, "Buzz");
         }
 
-        [TestMethod]
+        [Test]
         public void ShouldReturnFizzBuzzForNumber15()
         {
-            _fizzBuzzService.SetToDefaultValues();
             var result = _fizzBuzzService.GetValueForNumber(15);
             Assert.AreEqual(result, "FizzBuzz");
         }
 
-        [TestMethod]
+        [Test]
         public void ShouldReturn4ForNumber4()
         {
-            _fizzBuzzService.SetToDefaultValues();
             var result = _fizzBuzzService.GetValueForNumber(4);
             Assert.AreEqual(result, "4");
         }
 
-        [TestMethod]
-        public void ShouldReplaceDefaultFizzWord()
+        [Test]
+        public void ShouldOverwriteDefaultFizzWord()
         {
-            _fizzBuzzService.SetToDefaultValues();
             _fizzBuzzService.AddMap(3, "Fizzy");
             
             var result = _fizzBuzzService.GetValueForNumber(3);
@@ -58,19 +58,118 @@ namespace FizzBuzz.Tests
             Assert.AreEqual(result, "Fizzy");
         }
 
-        [TestMethod]
-        public void ShouldReturnArrayOfMaxNumberLength()
+        [Test]
+        public void ShouldAddNewWordNumberCombination()
         {
-            _fizzBuzzService.SetToDefaultValues();
+            _fizzBuzzService.AddMap(4, "Squirrel");
+
+            var result = _fizzBuzzService.GetValueForNumber(4);
+
+            Assert.AreEqual(result, "Squirrel");
+        }
+
+        [Test, ExpectedException(typeof (ArgumentOutOfRangeException))]
+        public void ShouldThrowExceptionIfMappedNumberIsNotInRange()
+        {
+            _fizzBuzzService.MinNumber = 10;
+            _fizzBuzzService.AddMap(5, "Monkey");
+        }
+
+        [Test]
+        public void ShouldReturnLastWordOnlyForDuplicateMaps()
+        {
+            _fizzBuzzService.AddMap(4, "Four4");
+            _fizzBuzzService.AddMap(4, "Quatre444");
+
+            _fizzBuzzService.MaxNumber = 5;
 
             var result = _fizzBuzzService.GetAllValues().ToList();
-            
+            Assert.AreEqual(result[3], "Quatre444");
+
+        }
+
+        [Test]
+        public void ShouldResetValuesToDefaults()
+        {
+          //Change all the possible properties
+            _fizzBuzzService.MinNumber = 3;
+            _fizzBuzzService.MaxNumber = 5;
+            _fizzBuzzService.AddMap(3, "Three3");
+            _fizzBuzzService.AddMap(4, "Four4");
+            _fizzBuzzService.AddMap(5, "Five5");
+
+            _fizzBuzzService.Reset();
+
+            Assert.AreEqual(_fizzBuzzService.MinNumber, 1);
+            Assert.AreEqual(_fizzBuzzService.MaxNumber, 100);
+
+            var result = _fizzBuzzService.GetAllValues().ToList();
+
             Assert.AreEqual(result.Count, 100);
             Assert.AreEqual(result[0], "1");
             Assert.AreEqual(result[2], "Fizz");
             Assert.AreEqual(result[4], "Buzz");
             Assert.AreEqual(result[14], "FizzBuzz");
         }
+
+     
+
+        /********************************************************************
+         * MinNumber and MaxNumber validations         
+         * ******************************************************************/
+        [Test, ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void ShouldGetExceptionIfNegativeNumberEnteredForMinNumber()
+        {
+            _fizzBuzzService.MinNumber = -100;
+        }
+
+        [Test, ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void ShouldGetExceptionIfNegativeNumberEnteredForMaxNumber()
+        {
+            _fizzBuzzService.MaxNumber = -100;
+        }
+
+        [Test, ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void ShouldGetExceptionIfMaxNumberLessThanMinNumber()
+        {
+            _fizzBuzzService.MaxNumber = 10;
+            _fizzBuzzService.MinNumber = 11;
+        }
+
+        [Test, ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void ShouldGetExceptionIfMinNumberGreaterThanMaxNumber()
+        {
+            _fizzBuzzService.MinNumber = 11;
+            _fizzBuzzService.MaxNumber = 10;
+
+        }
+
+        /*****************************************************************
+      * GetAllValues tests
+      * ***************************************************************/
+
+        [Test]
+        public void ShouldReturnArrayOf100Strings()
+        {
+            var result = _fizzBuzzService.GetAllValues().ToList();
+
+            Assert.AreEqual(result.Count, 100);
+            Assert.AreEqual(result[0], "1");
+            Assert.AreEqual(result[2], "Fizz");
+            Assert.AreEqual(result[4], "Buzz");
+            Assert.AreEqual(result[14], "FizzBuzz");
+        }
+
+        [Test]
+        public void ShouldReturnArrayOf7Strings()
+        {
+            _fizzBuzzService.MaxNumber = 7;
+            var result = _fizzBuzzService.GetAllValues().ToList();
+
+            Assert.AreEqual(result.Count, 7);
+        }
+
+
 
     }
 }
